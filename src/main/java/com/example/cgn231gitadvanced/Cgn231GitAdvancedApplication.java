4aa149173075cf48d6e9fadd602e21cc4b2b7692
valfink <valentin.finke@gmail.com>
2023-02-08 12:18:29 +0100

    Setup project

---
+++ src/main/java/com/example/cgn231gitadvanced/Cgn231GitAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgn231gitadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn231GitAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cgn231GitAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgn231gitadvanced/Cgn231GitAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgn231gitadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgn231GitAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
