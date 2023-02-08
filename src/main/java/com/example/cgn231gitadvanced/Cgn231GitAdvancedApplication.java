1d3374681f5ebe87a5840e7ba9c88e72377be54b
Hendrik Neuschulz <hneuhb@t-online.de>
2023-02-08 12:25:00 +0100

    .

--- src/main/java/com/example/cgn231gitadvanced/Cgn231GitAdvancedApplication.java
+++ src/main/java/com/example/cgn231gitadvanced/Cgn231GitAdvancedApplication.java
@@ -8,7 +8,7 @@ public class Cgn231GitAdvancedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Cgn231GitAdvancedApplication.class, args);
-        System.out.println("Hendrik war hier :)");
+        System.out.println("changes :)");
     }
 
 }
